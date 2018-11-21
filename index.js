61c36a4eaf063eb6bbcc0ce152fa99a647e07ac3
SoNar <gnaro93@gmail.com>
2018-11-21 11:56:41 +0400

    Mongo template

---
+++ index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+mongoose.connect('mongodb://localhost/youtube-video', { useNewUrlParser: true })
+    .then(() => console.log('Mongo started')
+    )
+    .catch(e => console.log(e))
+
+require('./person.model')
+const Person = mongoose.model('persons')
+
+const person = new Person({
+    name: 'Narek Sona Grigoryan',
+    age: 19,
+    isMarried: true,
+    phone: [093204507]
+})
+
+// http://mongoosejs.com/docs/queries.html
+
+// Person.find({name: {$in: ['Person1', 'Person2', 'Person3']}})
+// .sort('age')
+// .limit(2)
+// .then(persons => {
+//     console.log(JSON.stringify(persons, null, 2))
+// })
+
+Person.find({name: 'Narek Sona Grigoryan'})
+.sort('age')
+.then(persons => {
+    console.log(JSON.stringify(persons, null, 2))
+    const p = persons[0]
+
+  //  Person.find({_id: p.id}).deleteOne().then(_ => console.log('removed'))
+})
---
+++ person.model.js
@@ -0,0 +1,29 @@
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const PersonSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        min: 18,
+        max: 90,
+        default: 20
+    },
+    isMarried: {
+        type: Boolean,
+        default: false
+    },
+    phones: {
+        type: [Number],
+        default: []
+    }
+})
+
+mongoose.model('persons', PersonSchema)
+
+
+// [2323432, 23432423]
